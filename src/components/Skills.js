66e8a18ff88bacc2d96f4376dba2191ac22e8958
Brandon Fancher <brandonfancher@gmail.com>
2020-07-10 16:17:44 -0400

    Add hook-driven animations to skill progress bars.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,32 +1,41 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import SectionHeader from "./SectionHeader";
 
-// see main.js for source of skills animations
+const Skill = ({ skill, percent }) => {
+  const ref = useRef();
+  const onScreen = useOnScreen(ref, "-20px");
 
-const Skill = ({ skill, percent }) => (
-  <div className="o-grid__col-sm-6">
-    <div className="o-content">
-      <div className="o-media  o-media--block">
-        <div className="o-media__figure">
-          <div className="c-number  t-primary-color">
-            {percent}
-            <small>%</small>
+  return (
+    <div className="o-grid__col-sm-6">
+      <div className="o-content">
+        <div className="o-media  o-media--block">
+          <div className="o-media__figure">
+            <div className="c-number  t-primary-color">
+              {percent}
+              <small>%</small>
+            </div>
+          </div>
+          <div className="o-media__body">
+            <h3>{skill}</h3>
           </div>
         </div>
-        <div className="o-media__body">
-          <h3>{skill}</h3>
+        <div
+          ref={ref}
+          className="c-progress-bar  o-content__body  t-border-color-bg  u-mt-text"
+        >
+          <div
+            className={`a-progress-bar  c-progress-bar__filler  t-primary-bg ${
+              onScreen ? "in-view" : ""
+            }`}
+            style={{ width: `${onScreen ? percent : 0}%` }}
+            data-percent={percent}
+          />
         </div>
       </div>
-      <div className="c-progress-bar  o-content__body  t-border-color-bg  u-mt-text">
-        <div
-          className="a-progress-bar  c-progress-bar__filler  t-primary-bg"
-          data-percent={percent}
-        ></div>
-      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Skill.propTypes = {
   skill: PropTypes.string.isRequired,
@@ -60,3 +69,28 @@ const Skills = () => (
 );
 
 export default Skills;
+
+function useOnScreen(ref, rootMargin = "0px") {
+  // State and setter for storing whether element is visible
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // Update our state when observer callback fires
+        setIntersecting(entry.isIntersecting);
+      },
+      {
+        rootMargin,
+      }
+    );
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      observer.unobserve(ref.current);
+    };
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return isIntersecting;
+}
