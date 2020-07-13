907443b5b4b11d34c9366285a06a7fb95ad40fae
Brandon Fancher <brandonfancher@gmail.com>
2020-07-13 18:25:07 -0400

    Beginnings of a Portfolio section.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,19 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Section from "./Section";
-import portfolio1 from "../assets/images/portfolio/1_thumb.jpg";
-import portfolio2 from "../assets/images/portfolio/2_full.jpg";
-import portfolio3 from "../assets/images/portfolio/3_thumb.jpg";
-import portfolio4 from "../assets/images/portfolio/4_thumb.jpg";
-import portfolio5 from "../assets/images/portfolio/5_thumb.jpg";
-import portfolio6 from "../assets/images/portfolio/6_full.jpg";
+import rivkah from "../assets/images/portfolio/rivkah.jpg";
+import lightrail from "../assets/images/portfolio/lightrail.jpg";
 
-const PortfolioPiece = ({ title, description, image }) => (
+const PortfolioPiece = ({ title, description, link, image }) => (
   <div className="o-grid__col-sm-6">
     <a
-      className="c-image-overlay  t-image-overlay  js-lightbox"
-      href="#"
+      className="c-image-overlay t-image-overlay"
+      href={link}
       title={title}
+      target="_blank"
+      rel="noopener noreferrer"
     >
       <img src={image} alt="" />
       <div className="c-image-overlay__content">
@@ -28,57 +26,28 @@ const PortfolioPiece = ({ title, description, image }) => (
 PortfolioPiece.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
   image: PropTypes.any.isRequired,
 };
 
 const Portfolio = () => (
   <Section
     heading="Portfolio"
-    description="Placeholder"
+    description="More available on request."
     wrapperClasses="o-section__full-bottom-space"
   >
     <div className="o-grid  o-grid--gallery">
       <PortfolioPiece
-        title="Musée du Louvre"
-        description="Fusce lacinia arcu et nulla. Nulla vitae mauris non felis
-                    mollis faucibus. Lorem ipsum dolor sit amet, consectetuer
-                    adipiscing elit."
-        image={portfolio1}
+        title="Rivkah | Fine Art Photography"
+        description="Side Project: A responsive photography portfolio Jamstack site using React, Gatsby, Emotion, GraphQL, Prismic headless CMS, FlexSearch, and Netlify with CI/CD."
+        link="https://rivkahfineart.com"
+        image={rivkah}
       />
       <PortfolioPiece
-        title="Tunnel Effect"
-        description="Fusce lacinia arcu et nulla. Nulla vitae mauris non felis
-                    mollis faucibus. Lorem ipsum dolor sit amet, consectetuer
-                    adipiscing elit."
-        image={portfolio2}
-      />
-      <PortfolioPiece
-        title="New York, New York"
-        description="Fusce lacinia arcu et nulla. Nulla vitae mauris non felis
-                    mollis faucibus. Lorem ipsum dolor sit amet, consectetuer
-                    adipiscing elit."
-        image={portfolio3}
-      />
-      <PortfolioPiece
-        title="Sky High"
-        description="Fusce lacinia arcu et nulla. Nulla vitae mauris non felis
-                    mollis faucibus. Lorem ipsum dolor sit amet, consectetuer
-                    adipiscing elit."
-        image={portfolio4}
-      />
-      <PortfolioPiece
-        title="The Eiffel Tower"
-        description="Fusce lacinia arcu et nulla. Nulla vitae mauris non felis
-                    mollis faucibus. Lorem ipsum dolor sit amet, consectetuer
-                    adipiscing elit."
-        image={portfolio5}
-      />
-      <PortfolioPiece
-        title="Reaching the Clouds"
-        description="Fusce lacinia arcu et nulla. Nulla vitae mauris non felis
-                    mollis faucibus. Lorem ipsum dolor sit amet, consectetuer
-                    adipiscing elit."
-        image={portfolio6}
+        title="Charlotte Light Rail App"
+        description="Side Project: An open-source iOS transit app for the Charlotte community with thousands of monthly users. Built with React Native, React Navigation, Mapbox and Styled Components."
+        link="https://apps.apple.com/us/app/charlotte-light-rail/id1131471316"
+        image={lightrail}
       />
     </div>
   </Section>
